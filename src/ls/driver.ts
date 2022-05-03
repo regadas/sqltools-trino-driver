a5b61521142afa6c76e2797197551b25d7f91eac
regadas <filiperegadas@gmail.com>
2022-05-03 12:48:45 +0100

    Initial trino driver impl

--- src/ls/driver.ts
+++ src/ls/driver.ts
@@ -1,190 +1,224 @@
-import AbstractDriver from '@sqltools/base-driver';
-import queries from './queries';
-import { IConnectionDriver, MConnectionExplorer, NSDatabase, ContextValue, Arg0 } from '@sqltools/types';
-import { v4 as generateId } from 'uuid';
+import AbstractDriver from "@sqltools/base-driver";
+import queries from "./queries";
+import {
+  IConnectionDriver,
+  MConnectionExplorer,
+  NSDatabase,
+  ContextValue,
+  Arg0,
+} from "@sqltools/types";
+import { v4 as generateId } from "uuid";
+import presto from "presto-client";
 
-/**
- * set Driver lib to the type of your connection.
- * Eg for postgres:
- * import { Pool, PoolConfig } from 'pg';
- * ...
- * type DriverLib = Pool;
- * type DriverOptions = PoolConfig;
- *
- * This will give you completions iside of the library
- */
-type DriverLib = typeof fakeDbLib;
+type DriverLib = typeof presto.Client;
 type DriverOptions = any;
 
-/**
- * MOCKED DB DRIVER
- * THIS IS JUST AN EXAMPLE AND THE LINES BELOW SHOUDL BE REMOVED!
- */
-// import fakeDbLib from './mylib'; // this is what you should do
-const fakeDbLib = {
-  open: () => Promise.resolve(fakeDbLib),
-  query: (..._args: any[]) => {
-    const nResults = parseInt((Math.random() * 1000).toFixed(0));
-    const nCols = parseInt((Math.random() * 100).toFixed(0));
-    const colNames = [...new Array(nCols)].map((_, index) => `col${index}`);
-    const generateRow = () => {
-      const row = {};
-      colNames.forEach(c => {
-        row[c] = Math.random() * 1000;
-      });
-      return row;
-    }
-    const results = [...new Array(nResults)].map(generateRow);
-    return Promise.resolve([results]);
-  },
-  close: () => Promise.resolve(),
-};
-
-
-/* LINES ABOVE CAN BE REMOVED */
-
-
-export default class YourDriverClass extends AbstractDriver<DriverLib, DriverOptions> implements IConnectionDriver {
-
-  /**
-   * If you driver depends on node packages, list it below on `deps` prop.
-   * It will be installed automatically on first use of your driver.
-   */
-  public readonly deps: typeof AbstractDriver.prototype['deps'] = [{
-    type: AbstractDriver.CONSTANTS.DEPENDENCY_PACKAGE,
-    name: 'lodash',
-    // version: 'x.x.x',
-  }];
-
-
+export default class TrinoDriver
+  extends AbstractDriver<DriverLib, DriverOptions>
+  implements IConnectionDriver {
   queries = queries;
 
-  /** if you need to require your lib in runtime and then
-   * use `this.lib.methodName()` anywhere and vscode will take care of the dependencies
-   * to be installed on a cache folder
-   **/
-  // private get lib() {
-  //   return this.requireDep('node-packge-name') as DriverLib;
-  // }
-
-  public async open() {
+  public async open(): Promise<presto.Client> {
     if (this.connection) {
       return this.connection;
     }
 
-    this.needToInstallDependencies && await this.needToInstallDependencies();
-    /**
-     * open your connection here!!!
-     */
+    const connOptions = {
+      host: this.credentials.host,
+      port: 8080,
+      catalog: this.credentials.catalog,
+      schema: this.credentials.schema,
+      user: this.credentials.user,
+      engine: "trino",
+      source: "sqltools-driver",
+    };
 
-    this.connection = fakeDbLib.open();
+    try {
+      const conn = new presto.Client(connOptions);
+      this.connection = Promise.resolve(conn);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+
     return this.connection;
   }
 
   public async close() {
     if (!this.connection) return Promise.resolve();
-    /**
-     * cose you connection here!!
-     */
-    await fakeDbLib.close();
+    await this.connection;
     this.connection = null;
   }
 
-  public query: (typeof AbstractDriver)['prototype']['query'] = async (queries, opt = {}) => {
-    const db = await this.open();
-    const queriesResults = await db.query(queries);
-    const resultsAgg: NSDatabase.IResult[] = [];
-    queriesResults.forEach(queryResult => {
-      resultsAgg.push({
-        cols: Object.keys(queryResult[0]),
-        connId: this.getId(),
-        messages: [{ date: new Date(), message: `Query ok with ${queriesResults.length} results`}],
-        results: queryResult,
-        query: queries.toString(),
-        requestId: opt.requestId,
-        resultId: generateId(),
+  public query: typeof AbstractDriver["prototype"]["query"] = async (
+    query,
+    opt = {}
+  ) => {
+    const { requestId } = opt;
+    return this.open()
+      .then(async (db) => {
+        const queriesResults: any[] = await new Promise((resolve, reject) => {
+          let results = [];
+          db.execute({
+            query: query,
+            data: function (error, data, columns, _) {
+              if (error) return reject(error);
+
+              const d = {};
+              data.forEach((row) => {
+                columns.forEach((element, ci) => (d[element.name] = row[ci]));
+              });
+
+              results.push(d);
+            },
+            success: function (error, _) {
+              if (error) return reject(error);
+              resolve(results);
+            },
+            error: function (error) {
+              reject(error);
+            },
+          });
+        });
+        return queriesResults;
+      })
+      .then((results) => {
+        return [
+          <NSDatabase.IResult>{
+            requestId,
+            resultId: generateId(),
+            connId: this.getId(),
+            cols: Object.keys(results[0]),
+            messages: [
+              this.prepareMessage(
+                [`Successfully executed. ${results.length} rows were affected.`]
+                  .filter(Boolean)
+                  .join(" ")
+              ),
+            ],
+            query,
+            results,
+          },
+        ];
+      })
+      .catch((error) => {
+        return [
+          <NSDatabase.IResult>{
+            connId: this.getId(),
+            requestId,
+            resultId: generateId(),
+            cols: [],
+            messages: [
+              this.prepareMessage(
+                [error.message.replace(/\n/g, " ")].filter(Boolean).join(" ")
+              ),
+            ],
+            error: true,
+            rawError: error,
+            query,
+            results: [],
+          },
+        ];
       });
-    });
-    /**
-     * write the method to execute queries here!!
-     */
-    return resultsAgg;
-  }
+  };
 
   /** if you need a different way to test your connection, you can set it here.
    * Otherwise by default we open and close the connection only
    */
   public async testConnection() {
     await this.open();
-    await this.query('SELECT 1', {});
+    const testSelect = await this.query("SELECT 1", {});
+    if (testSelect[0].error) {
+      return Promise.reject({
+        message: `Connected but cannot run SQL. ${testSelect[0].rawError}`,
+      });
+    }
   }
 
   /**
    * This method is a helper to generate the connection explorer tree.
    * it gets the child items based on current item
    */
-  public async getChildrenForItem({ item, parent }: Arg0<IConnectionDriver['getChildrenForItem']>) {
+  public async getChildrenForItem({
+    item,
+    parent,
+  }: Arg0<IConnectionDriver["getChildrenForItem"]>) {
     switch (item.type) {
       case ContextValue.CONNECTION:
       case ContextValue.CONNECTED_CONNECTION:
         return <MConnectionExplorer.IChildItem[]>[
-          { label: 'Tables', type: ContextValue.RESOURCE_GROUP, iconId: 'folder', childType: ContextValue.TABLE },
-          { label: 'Views', type: ContextValue.RESOURCE_GROUP, iconId: 'folder', childType: ContextValue.VIEW },
+          {
+            label: "Tables",
+            type: ContextValue.RESOURCE_GROUP,
+            iconId: "folder",
+            childType: ContextValue.TABLE,
+          },
+          {
+            label: "Views",
+            type: ContextValue.RESOURCE_GROUP,
+            iconId: "folder",
+            childType: ContextValue.VIEW,
+          },
         ];
       case ContextValue.TABLE:
       case ContextValue.VIEW:
         let i = 0;
-        return <NSDatabase.IColumn[]>[{
-          database: 'fakedb',
-          label: `column${i++}`,
-          type: ContextValue.COLUMN,
-          dataType: 'faketype',
-          schema: 'fakeschema',
-          childType: ContextValue.NO_CHILD,
-          isNullable: false,
-          iconName: 'column',
-          table: parent,
-        },{
-          database: 'fakedb',
-          label: `column${i++}`,
-          type: ContextValue.COLUMN,
-          dataType: 'faketype',
-          schema: 'fakeschema',
-          childType: ContextValue.NO_CHILD,
-          isNullable: false,
-          iconName: 'column',
-          table: parent,
-        },{
-          database: 'fakedb',
-          label: `column${i++}`,
-          type: ContextValue.COLUMN,
-          dataType: 'faketype',
-          schema: 'fakeschema',
-          childType: ContextValue.NO_CHILD,
-          isNullable: false,
-          iconName: 'column',
-          table: parent,
-        },{
-          database: 'fakedb',
-          label: `column${i++}`,
-          type: ContextValue.COLUMN,
-          dataType: 'faketype',
-          schema: 'fakeschema',
-          childType: ContextValue.NO_CHILD,
-          isNullable: false,
-          iconName: 'column',
-          table: parent,
-        },{
-          database: 'fakedb',
-          label: `column${i++}`,
-          type: ContextValue.COLUMN,
-          dataType: 'faketype',
-          schema: 'fakeschema',
-          childType: ContextValue.NO_CHILD,
-          isNullable: false,
-          iconName: 'column',
-          table: parent,
-        }];
+        return <NSDatabase.IColumn[]>[
+          {
+            database: "fakedb",
+            label: `column${i++}`,
+            type: ContextValue.COLUMN,
+            dataType: "faketype",
+            schema: "fakeschema",
+            childType: ContextValue.NO_CHILD,
+            isNullable: false,
+            iconName: "column",
+            table: parent,
+          },
+          {
+            database: "fakedb",
+            label: `column${i++}`,
+            type: ContextValue.COLUMN,
+            dataType: "faketype",
+            schema: "fakeschema",
+            childType: ContextValue.NO_CHILD,
+            isNullable: false,
+            iconName: "column",
+            table: parent,
+          },
+          {
+            database: "fakedb",
+            label: `column${i++}`,
+            type: ContextValue.COLUMN,
+            dataType: "faketype",
+            schema: "fakeschema",
+            childType: ContextValue.NO_CHILD,
+            isNullable: false,
+            iconName: "column",
+            table: parent,
+          },
+          {
+            database: "fakedb",
+            label: `column${i++}`,
+            type: ContextValue.COLUMN,
+            dataType: "faketype",
+            schema: "fakeschema",
+            childType: ContextValue.NO_CHILD,
+            isNullable: false,
+            iconName: "column",
+            table: parent,
+          },
+          {
+            database: "fakedb",
+            label: `column${i++}`,
+            type: ContextValue.COLUMN,
+            dataType: "faketype",
+            schema: "fakeschema",
+            childType: ContextValue.NO_CHILD,
+            isNullable: false,
+            iconName: "column",
+            table: parent,
+          },
+        ];
       case ContextValue.RESOURCE_GROUP:
         return this.getChildrenForGroup({ item, parent });
     }
@@ -195,32 +229,38 @@ export default class YourDriverClass extends AbstractDriver<DriverLib, DriverOpt
    * This method is a helper to generate the connection explorer tree.
    * It gets the child based on child types
    */
-  private async getChildrenForGroup({ parent, item }: Arg0<IConnectionDriver['getChildrenForItem']>) {
+  private async getChildrenForGroup({
+    parent,
+    item,
+  }: Arg0<IConnectionDriver["getChildrenForItem"]>) {
     console.log({ item, parent });
     switch (item.childType) {
       case ContextValue.TABLE:
       case ContextValue.VIEW:
         let i = 0;
-        return <MConnectionExplorer.IChildItem[]>[{
-          database: 'fakedb',
-          label: `${item.childType}${i++}`,
-          type: item.childType,
-          schema: 'fakeschema',
-          childType: ContextValue.COLUMN,
-        },{
-          database: 'fakedb',
-          label: `${item.childType}${i++}`,
-          type: item.childType,
-          schema: 'fakeschema',
-          childType: ContextValue.COLUMN,
-        },
-        {
-          database: 'fakedb',
-          label: `${item.childType}${i++}`,
-          type: item.childType,
-          schema: 'fakeschema',
-          childType: ContextValue.COLUMN,
-        }];
+        return <MConnectionExplorer.IChildItem[]>[
+          {
+            database: "fakedb",
+            label: `${item.childType}${i++}`,
+            type: item.childType,
+            schema: "fakeschema",
+            childType: ContextValue.COLUMN,
+          },
+          {
+            database: "fakedb",
+            label: `${item.childType}${i++}`,
+            type: item.childType,
+            schema: "fakeschema",
+            childType: ContextValue.COLUMN,
+          },
+          {
+            database: "fakedb",
+            label: `${item.childType}${i++}`,
+            type: item.childType,
+            schema: "fakeschema",
+            childType: ContextValue.COLUMN,
+          },
+        ];
     }
     return [];
   }
@@ -228,91 +268,103 @@ export default class YourDriverClass extends AbstractDriver<DriverLib, DriverOpt
   /**
    * This method is a helper for intellisense and quick picks.
    */
-  public async searchItems(itemType: ContextValue, search: string, _extraParams: any = {}): Promise<NSDatabase.SearchableItem[]> {
+  public async searchItems(
+    itemType: ContextValue,
+    search: string,
+    _extraParams: any = {}
+  ): Promise<NSDatabase.SearchableItem[]> {
     switch (itemType) {
       case ContextValue.TABLE:
       case ContextValue.VIEW:
         let j = 0;
-        return [{
-          database: 'fakedb',
-          label: `${search || 'table'}${j++}`,
-          type: itemType,
-          schema: 'fakeschema',
-          childType: ContextValue.COLUMN,
-        },{
-          database: 'fakedb',
-          label: `${search || 'table'}${j++}`,
-          type: itemType,
-          schema: 'fakeschema',
-          childType: ContextValue.COLUMN,
-        },
-        {
-          database: 'fakedb',
-          label: `${search || 'table'}${j++}`,
-          type: itemType,
-          schema: 'fakeschema',
-          childType: ContextValue.COLUMN,
-        }]
+        return [
+          {
+            database: "fakedb",
+            label: `${search || "table"}${j++}`,
+            type: itemType,
+            schema: "fakeschema",
+            childType: ContextValue.COLUMN,
+          },
+          {
+            database: "fakedb",
+            label: `${search || "table"}${j++}`,
+            type: itemType,
+            schema: "fakeschema",
+            childType: ContextValue.COLUMN,
+          },
+          {
+            database: "fakedb",
+            label: `${search || "table"}${j++}`,
+            type: itemType,
+            schema: "fakeschema",
+            childType: ContextValue.COLUMN,
+          },
+        ];
       case ContextValue.COLUMN:
         let i = 0;
         return [
           {
-            database: 'fakedb',
-            label: `${search || 'column'}${i++}`,
+            database: "fakedb",
+            label: `${search || "column"}${i++}`,
             type: ContextValue.COLUMN,
-            dataType: 'faketype',
-            schema: 'fakeschema',
+            dataType: "faketype",
+            schema: "fakeschema",
             childType: ContextValue.NO_CHILD,
             isNullable: false,
-            iconName: 'column',
-            table: 'fakeTable'
-          },{
-            database: 'fakedb',
-            label: `${search || 'column'}${i++}`,
+            iconName: "column",
+            table: "fakeTable",
+          },
+          {
+            database: "fakedb",
+            label: `${search || "column"}${i++}`,
             type: ContextValue.COLUMN,
-            dataType: 'faketype',
-            schema: 'fakeschema',
+            dataType: "faketype",
+            schema: "fakeschema",
             childType: ContextValue.NO_CHILD,
             isNullable: false,
-            iconName: 'column',
-            table: 'fakeTable'
-          },{
-            database: 'fakedb',
-            label: `${search || 'column'}${i++}`,
+            iconName: "column",
+            table: "fakeTable",
+          },
+          {
+            database: "fakedb",
+            label: `${search || "column"}${i++}`,
             type: ContextValue.COLUMN,
-            dataType: 'faketype',
-            schema: 'fakeschema',
+            dataType: "faketype",
+            schema: "fakeschema",
             childType: ContextValue.NO_CHILD,
             isNullable: false,
-            iconName: 'column',
-            table: 'fakeTable'
-          },{
-            database: 'fakedb',
-            label: `${search || 'column'}${i++}`,
+            iconName: "column",
+            table: "fakeTable",
+          },
+          {
+            database: "fakedb",
+            label: `${search || "column"}${i++}`,
             type: ContextValue.COLUMN,
-            dataType: 'faketype',
-            schema: 'fakeschema',
+            dataType: "faketype",
+            schema: "fakeschema",
             childType: ContextValue.NO_CHILD,
             isNullable: false,
-            iconName: 'column',
-            table: 'fakeTable'
-          },{
-            database: 'fakedb',
-            label: `${search || 'column'}${i++}`,
+            iconName: "column",
+            table: "fakeTable",
+          },
+          {
+            database: "fakedb",
+            label: `${search || "column"}${i++}`,
             type: ContextValue.COLUMN,
-            dataType: 'faketype',
-            schema: 'fakeschema',
+            dataType: "faketype",
+            schema: "fakeschema",
             childType: ContextValue.NO_CHILD,
             isNullable: false,
-            iconName: 'column',
-            table: 'fakeTable'
-          }
+            iconName: "column",
+            table: "fakeTable",
+          },
         ];
     }
     return [];
   }
 
-  public getStaticCompletions: IConnectionDriver['getStaticCompletions'] = async () => {
-    return {};
-  }
+  public getStaticCompletions: IConnectionDriver["getStaticCompletions"] =
+    async () => {
+      return {};
+    };
 }
