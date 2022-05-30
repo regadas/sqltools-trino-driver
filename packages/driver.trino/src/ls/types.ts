c7def7d62165fd4fb15c57360a437ae6c75e6b57
regadas <oss@regadas.email>
2022-05-30 11:52:45 +0100

    Cleanup query execution

--- packages/driver.trino/src/ls/types.ts
+++ packages/driver.trino/src/ls/types.ts
@@ -1,4 +1,4 @@
 type Query = string;
 type Rows = Array<{ [key: string]: any }>;
 type Columns = Array<{ name: string; type: string }>;
-export type QueryResponse = [Query, Rows, Columns];
+export type QueryResult = [Query, Rows, Columns];
